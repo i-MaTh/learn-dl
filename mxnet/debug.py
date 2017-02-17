4883daff81406871db04d9d1fb9ea9f174c3153b
i-MaTh <izhangwenjie@vip.qq.com>
2017-02-17 13:55:37 +0800

    Create debug.py

---
+++ mxnet/debug.py
@@ -0,0 +1,9 @@
+import mxnet as mx
+
+
+input_x = mx.sym.Variable('input_x')
+embed_layer = mx.sym.Embedding(data=input_x, input_dim=26, output_dim=10, name='vocab_embed')
+
+# infer_shape
+input_shape = {'input_x' : (2,1)} # choose it by yourself.
+print c.infer_shape(**input_shape) 
